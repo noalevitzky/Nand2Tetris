55ca01cf0b876a55efb47cccc6fd68c069a40ae8
Giladsid <57175818+Giladsid@users.noreply.github.com>
2019-11-25 10:11:46 +0200

    Update VMtranslator.py

--- 07/VMtranslator.py
+++ 07/VMtranslator.py
@@ -6,39 +6,43 @@ import sys
 # _____________ VMtranslator Program _____________ #
 
 
-def write_file(file, cw):
+def write_file(file_str, cw):
     """
-    :param file: input file
+    :param file_str: input file
     :param cw: code writer object
     write command into the output file, using the cw
     """
-    parser = Parser.Parser(filename)
-    cw.set_file_name(file)
+    parser = Parser.Parser(file_str)
+    cw.set_file_name(file_str)
 
     while parser.has_more_commands():
         parser.advance()
         c_type = parser.command_type()
 
-        if c_type == Parser.C_PUSH or c_type == Parser.C_POP:
+        if c_type == "C_PUSH" or c_type == "C_POP":
             # write push / pop command
             segment = parser.arg1()
             index = parser.arg2()
             cw.write_push_pop(c_type, segment, index)
-        elif c_type == Parser.C_ARITHMETIC:
+        elif c_type == "C_ARITHMETIC":
             # write arithmetic command
             command = parser.arg1()
             cw.write_arithmetic(command)
 
 
-if __name__ == "_main__":
+
+if __name__ == '__main__':
     """
     translates VM file / directory to asm file
     """
-    path = sys.argv[1]
-    outfile = path.replace(".vm", ".asm")
-    code_writer = CodeWriter(outfile)
+    path_name = sys.argv[1]
+    path = Path(path_name)
 
-    if Path.is_dir(path):
+    # create outfile
+    outfile = path_name.replace(".vm", ".asm")
+    code_writer = CodeWriter.CodeWriter(outfile)
+
+    if path.is_dir():
         # path is a directory
         for filename in path.iterdir():
             write_file(filename, code_writer)
@@ -49,6 +53,3 @@ if __name__ == "_main__":
 
     # close output file
     code_writer.close()
-
-
-
