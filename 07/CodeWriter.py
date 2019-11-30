c0f6788c61d6b32b6c8971b38f556edf19bc3bd9
Giladsid <57175818+Giladsid@users.noreply.github.com>
2019-11-30 15:58:21 +0200

    Update CodeWriter.py
    Updated version, T1 test left to be handled

--- 07/CodeWriter.py
+++ 07/CodeWriter.py
@@ -4,13 +4,66 @@ TEMP = "temp"
 POINTER = "pointer"
 STATIC = "static"
 CALC = "13"
+CLOSING_BRACKET = ")"
+
+# _______________ Assembly Commands _______________#
+
+# _____M Commands______ #
+M_MINUS_1 = "M=M-1"
+M_PLUS_1 = "M=M+1"
+M_ZERO = "M=0"
+M_NEGATE = "M=-M"
+M_NOT = "M=!M"
+M_IS_MINUS_1 = "M=-1"
+M_D = "M=D"
+M_PLUS_D = "M=M+D"
+M_MINUS_D = "M=M-D"
+M_M_AND_D = "M=M&D"
+M_OR_D = "M=M|D"
+
+# _____ A Commands______ #
+A_M = "A=M"
+A_M_PLUS_D = "A=M+D"
+
+# _____D Commands______ #
+D_A = "D=A"
+D_M = "D=M"
+D_M_MINUS_D = "D=M-D"
+D_MINUS_M = "D=D-M"
+
+# _____@ Commands______ #
+AT = "@"
+AT_SP = "@SP"
+AT_FUNC = "@FUNC"
+AT_TRUE = "@TRUE"
+AT_FALSE = "@FALSE"
+AT_TEMP = "@R14"
+AT_Y_LE = "@Y_LE"
+AT_Y_GT = "@Y_GT"
+AT_END = "@END"
+
+# _____Jump Commands______ #
+JMP = "0;JMP"
+JEQ = "D;JEQ"
+JGT = "D;JGT"
+JGE = "D;JGE"
+JLT = "D;JLT"
+JLE = "D;JLE"
+JNE = "D;JNE"
+
+# _____Boolean & function Commands______ #
+FALSE = "(FALSE"
+TRUE = "(TRUE"
+FUNC = "(FUNC"
+Y_GT = "(Y_GT"
+Y_LE = "(Y_LE"
+END = "(END)"
 
 
 # _____________ CodeWriter Class _____________ #
 
 # Translates VM commands into Hack assembly code
 class CodeWriter:
-
     # dict for converting ram segments to ram indices
     __ram_dict = {
         "local": 1,
@@ -28,7 +81,7 @@ class CodeWriter:
         :param out_file: The output file that's being written
         """
         self.out_file = open(out_file, "w+")
-        self.f_name = out_file.replace(".hack", "")     # name for statics vars
+        self.f_name = out_file.replace(".hack", "")  # name for statics vars
         self.stack = []
         self.boolean_counter = 0
 
@@ -48,143 +101,216 @@ class CodeWriter:
         """
         if command == "add":
             self._write_add()
+            return
         elif command == "sub":
             self._write_sub()
+            return
         elif command == "neg":
             self._write_negate()
-        elif command == "eq":
-            self._write_eq()
-            self._write_false()
-            self._write_true()
-            self._write_func()
-            self.boolean_counter += 1
-        elif command == "gt":
-            self._write_gt()
-            self._write_false()
-            self._write_true()
-            self._write_func()
-            self.boolean_counter += 1
-        elif command == "lt":
-            self._write_lt()
-            self._write_false()
-            self._write_true()
-            self._write_func()
-            self.boolean_counter += 1
+            return
         elif command == "and":
+            print("entered and")
             self._write_and()
+            return
         elif command == "or":
             self._write_or()
+            return
         elif command == "not":
             self._write_not()
+            return
+        elif command == "eq":
+            self._write_eq()
+        elif command == "gt":
+            self._write_gt()
+        elif command == "lt":
+            self._write_lt()
+        self._write_false()
+        self._write_true()
+        self._write_func()
+        self.boolean_counter += 1
+
 
     def _write_negate(self):
-        self.out_file.write("@SP" + "\n")
-        self.out_file.write("M=M-1" + "\n")
-        self.out_file.write("A=M" + "\n")
-        self.out_file.write("M=-M" + "\n")
-        self.out_file.write("@SP" + "\n")
-        self.out_file.write("M=M+1" + "\n")
+        """
+        Writes the negation of the stack's last item
+        """
+        cmd_block = []
+        # fill the cmd_block with the rest of the relevant commands
+        cmd_block.extend([AT_SP, M_MINUS_1, A_M, M_NEGATE, AT_SP, M_PLUS_1])
+        # write the commands to the out file
+        self.write_block(cmd_block)
+
 
     def _write_add(self):
-        self._pop_stack_to_d()
-        self.out_file.write("@SP" + "\n")
-        self.out_file.write("M=M-1" + "\n")
-        self.out_file.write("A=M" + "\n")
-        self.out_file.write("M=M+D" + "\n")
-        self.out_file.write("@SP" + "\n")
-        self.out_file.write("M=M+1" + "\n")
+        """
+        Writes the addition of the stack's last two item
+        """
+        cmd_block = self._pop_stack_to_d([])
+        # fill the cmd_block with the rest of the relevant commands
+        cmd_block.extend([AT_SP, M_MINUS_1, A_M, M_PLUS_D, AT_SP, M_PLUS_1])
+        # write the commands to the out file
+        self.write_block(cmd_block)
+
 
     def _write_false(self):
-        self.out_file.write("(FALSE" + str(self.boolean_counter) + ")" + "\n")
-        self.out_file.write("@SP" + "\n")
-        self.out_file.write("A=M" + "\n")
-        self.out_file.write("M=0" + "\n")
-        self.out_file.write("@SP" + "\n")
-        self.out_file.write("M=M+1" + "\n")
-        self.out_file.write("@FUNC" + str(self.boolean_counter) + "\n")
-        self.out_file.write("0;JMP" + "\n\n")
+        """
+        Writes a false function
+        """
+        cmd_block = []
+        # get the current false function number
+        current_false = self.get_current_func_name(FALSE)
+        current_func = AT_FUNC + str(self.boolean_counter)
+        # write the commands to the out file
+        cmd_block.extend(
+            [current_false, AT_SP, A_M, M_ZERO, AT_SP, M_PLUS_1, current_func,
+             JMP])
+        self.write_block(cmd_block)
+
+
+    def get_current_func_name(self, func_name):
+        """
+        Completes the string of the given func_name according the the
+        current boolean counter
+        :param func_name: the name of the given function
+        :return: The completed function name
+        """
+        return func_name + str(self.boolean_counter) + CLOSING_BRACKET
 
     def _write_func(self):
-        self.out_file.write("(FUNC" + str(self.boolean_counter) + ")" + "\n")
+        """
+        Writes the current function's name
+        """
+        self.write_block([self.get_current_func_name(FUNC)])
 
     def _write_true(self):
-        self.out_file.write("(TRUE" + str(self.boolean_counter) + ")" + "\n")
-        self.out_file.write("@SP" + "\n")
-        self.out_file.write("A=M" + "\n")
-        self.out_file.write("M=-1" + "\n")
-        self.out_file.write("@SP" + "\n")
-        self.out_file.write("M=M+1" + "\n")
-        self.out_file.write("@FUNC" + str(self.boolean_counter) + "\n")
-        self.out_file.write("0;JMP" + "\n\n")
+        """
+        Writes a current "true" loop
+        """
+        cmd_block = []
+        # get the current true loop name
+        current_true = self.get_current_func_name(TRUE)
+        # get the current func loop name
+        current_func = AT_FUNC + str(self.boolean_counter)
+        cmd_block.extend(
+            [current_true, AT_SP, A_M, M_IS_MINUS_1, AT_SP, M_PLUS_1,
+             current_func,
+             JMP])
+        # write the commands to the out file
+        self.write_block(cmd_block)
+
 
     def _write_eq(self):
-        self._write_bool()
-        self.out_file.write("@TRUE" + str(self.boolean_counter) + "\n")
-        self.out_file.write("D;JEQ" + "\n")
-        self.out_file.write("@FALSE" + str(self.boolean_counter) + "\n")
-        self.out_file.write("D;JNE" + "\n")
+
+        cmd_block = self._get_bool([])
+        current_true = AT_TRUE + str(self.boolean_counter)
+        current_false = AT_FALSE + str(self.boolean_counter)
+        cmd_block.extend([current_true, JEQ, current_false, JNE])
+        self.write_block(cmd_block)
+
 
     def _write_lt(self):
-        self._write_bool()
-        self.out_file.write("@TRUE" + str(self.boolean_counter) + "\n")
-        self.out_file.write("D;JLT" + "\n")
-        self.out_file.write("@FALSE" + str(self.boolean_counter) + "\n")
-        self.out_file.write("D;JGE" + "\n")
+        cmd_block = self._pop_stack_to_d([])
+        cmd_block.extend([AT_TEMP, M_D])
+        curr_func_ygt = AT_Y_GT + str(self.boolean_counter)
+        curr_func_yle = AT_Y_LE + str(self.boolean_counter)
+        # get the fitting commands for the ylt loop and yge loop
+        curr_loop_ygt = self.lt_get_ygt_loop(AT_TRUE + str(self.boolean_counter),
+                                             AT_FALSE + str(self.boolean_counter))
+        curr_loop_yle = self.lt_get_yle_loop(AT_TRUE + str(self.boolean_counter),
+                                             AT_FALSE + str(self.boolean_counter))
+        cmd_block.extend([curr_func_ygt, JGT, curr_func_yle, JLE])
+        cmd_block.extend(curr_loop_ygt)
+        cmd_block.extend(curr_loop_yle)
+        self.write_block(cmd_block)
+
+    def lt_get_ygt_loop(self, curr_true, curr_false):
+        cmd_block = [self.get_current_func_name(Y_GT)]
+        cmd_block.extend(self._pop_stack_to_d([]))
+        cmd_block.extend(
+            [curr_true, JLE, AT_TEMP, D_MINUS_M, curr_true, JLT, curr_false,
+             JGE])
+        return cmd_block
+
+    def lt_get_yle_loop(self, curr_true, curr_false):
+        cmd_block = [self.get_current_func_name(Y_LE)]
+        cmd_block.extend(self._pop_stack_to_d([]))
+        cmd_block.extend(
+            [curr_false, JGT, AT_TEMP, D_MINUS_M, curr_true, JLT, curr_false,
+             JGE])
+        return cmd_block
 
     def _write_gt(self):
-        self._write_bool()
-        self.out_file.write("@TRUE" + str(self.boolean_counter) + "\n")
-        self.out_file.write("D;JGT" + "\n")
-        self.out_file.write("@FALSE" + str(self.boolean_counter) + "\n")
-        self.out_file.write("D;JLE" + "\n")
+        cmd_block = self._pop_stack_to_d([])
+        cmd_block.extend([AT_TEMP, M_D])
+        curr_func_ygt = AT_Y_GT + str(self.boolean_counter)
+        curr_func_yle = AT_Y_LE + str(self.boolean_counter)
+        # get the fitting commands for the ygt loop and yle loop
+        curr_loop_ygt = self.gt_get_ygt_loop(AT_TRUE + str(self.boolean_counter),
+                                             AT_FALSE + str(self.boolean_counter))
+        curr_loop_yle = self.gt_get_yle_loop(AT_TRUE + str(self.boolean_counter),
+                                             AT_FALSE + str(self.boolean_counter))
+        cmd_block.extend(
+            [curr_func_ygt, JGT, curr_func_yle, JLE])
+        cmd_block.extend(curr_loop_ygt)
+        cmd_block.extend(curr_loop_yle)
+        self.write_block(cmd_block)
+
+    def gt_get_ygt_loop(self, curr_true, curr_false):
+        cmd_block = [self.get_current_func_name(Y_GT)]
+        cmd_block.extend(self._pop_stack_to_d([]))
+        cmd_block.extend(
+            [curr_false, JLE, AT_TEMP, D_MINUS_M, curr_true, JGT, curr_false,
+             JLE])
+        return cmd_block
+
+    def gt_get_yle_loop(self, curr_true, curr_false):
+        cmd_block = [self.get_current_func_name(Y_LE)]
+        cmd_block.extend(self._pop_stack_to_d([]))
+        cmd_block.extend(
+            [curr_true, JGT, AT_TEMP, D_MINUS_M, curr_true, JGT, curr_false,
+             JLE])
+        return cmd_block
+
 
     def _write_not(self):
-        self.out_file.write("@SP" + "\n")
-        self.out_file.write("M=M-1" + "\n")
-        self.out_file.write("A=M" + "\n")
-        self.out_file.write("M=!M" + "\n")
-        self.out_file.write("@SP" + "\n")
-        self.out_file.write("M=M+1" + "\n")
+        cmd_block = []
+        cmd_block.extend([AT_SP, M_MINUS_1, A_M, M_NOT, AT_SP, M_PLUS_1])
+        self.write_block(cmd_block)
+
 
     def _write_and(self):
-        self._pop_stack_to_d()
-        self.out_file.write("@SP" + "\n")
-        self.out_file.write("M=M-1" + "\n")
-        self.out_file.write("A=M" + "\n")
-        self.out_file.write("M=M&D" + "\n")
-        self.out_file.write("@SP" + "\n")
-        self.out_file.write("M=M+1" + "\n")
+        cmd_block = self._pop_stack_to_d([])
+        cmd_block.extend([AT_SP, M_MINUS_1,A_M, M_M_AND_D, AT_SP, M_PLUS_1])
+        self.write_block(cmd_block)
 
     def _write_or(self):
-        self._pop_stack_to_d()
-        self.out_file.write("@SP" + "\n")
-        self.out_file.write("M=M-1" + "\n")
-        self.out_file.write("A=M" + "\n")
-        self.out_file.write("M=M|D" + "\n")
-        self.out_file.write("@SP" + "\n")
-        self.out_file.write("M=M+1" + "\n")
+        cmd_block = self._pop_stack_to_d([])
+        cmd_block.extend([AT_SP, M_MINUS_1, A_M, M_OR_D, AT_SP, M_PLUS_1])
+        self.write_block(cmd_block)
 
-    def _write_bool(self):
-        self._pop_stack_to_d()
-        self.out_file.write("@SP" + "\n")
-        self.out_file.write("M=M-1" + "\n")
-        self.out_file.write("A=M" + "\n")
-        self.out_file.write("D=M-D" + "\n")
+
+    def _get_bool(self, cmd_block):
+        curr_block = self._pop_stack_to_d(cmd_block)
+        curr_block.extend([AT_SP, M_MINUS_1, A_M, D_M_MINUS_D])
+        return curr_block
+
 
     def _write_sub(self):
-        self._pop_stack_to_d()
-        self.out_file.write("@SP" + "\n")
-        self.out_file.write("M=M-1" + "\n")
-        self.out_file.write("A=M" + "\n")
-        self.out_file.write("M=M-D" + "\n")
-        self.out_file.write("@SP" + "\n")
-        self.out_file.write("M=M+1" + "\n")
+        cmd_block = self._pop_stack_to_d([])
+        cmd_block.extend([AT_SP, M_MINUS_1, A_M, M_MINUS_D, AT_SP, M_PLUS_1])
+        self.write_block(cmd_block)
 
-    def _pop_stack_to_d(self):
-        self.out_file.write("@SP" + "\n")
-        self.out_file.write("M=M-1" + "\n")
-        self.out_file.write("A=M" + "\n")
-        self.out_file.write("D=M" + "\n")
+
+    def _pop_stack_to_d(self, cmd_block):
+        """
+        Adds assembly commands that pop the stack's last item to D.
+        :param cmd_block: List of the Assembly commands
+        :return: A list of the given Assembly followed by popping the
+         stack's last item to D
+        """
+        cmd_block.extend([AT_SP, M_MINUS_1, A_M, D_M])
+        return cmd_block
+
 
     def write_push_pop(self, command, segment, index):
         """
@@ -200,13 +326,17 @@ class CodeWriter:
 
     def _read_from_A(self, index):
         """ reads the given index as A """
-        self.out_file.write("@" + str(index) + "\n")
-        self.out_file.write("D=A" + "\n")
+        cmd_block = []
+        cmd_block.extend([AT+str(index), D_A])
+        self.write_block(cmd_block)
+
 
     def _read_from_address(self, address):
         """ reads the given index as M (from R at address) """
-        self.out_file.write("@" + str(address) + "\n")
-        self.out_file.write("D=M" + "\n")
+        cmd_block = []
+        cmd_block.extend([AT+str(address), D_M])
+        self.write_block(cmd_block)
+
 
     def _write_push(self, segment, index):
         """
@@ -229,22 +359,22 @@ class CodeWriter:
         else:
             # copy content from address held in pointer to D
             self._read_from_address(self.__ram_dict[segment])
-            self.out_file.write("@" + CALC + "\n")
-            self.out_file.write("M=D" + "\n")
+            cmd_block_1,cmd_block_2 = [], []
+            cmd_block_1.extend([AT+CALC, M_D])
+            self.write_block(cmd_block_1)
+
             self._read_from_A(index)
-            self.out_file.write("@" + CALC + "\n")
-            self.out_file.write("A=M+D" + "\n")
-            self.out_file.write("D=M" + "\n")
+            cmd_block_2.extend([AT+CALC, A_M_PLUS_D, D_M])
+            self.write_block(cmd_block_2)
 
         # push D content to stack
         self._push_D()
 
     def _push_D(self):
-        self.out_file.write("@SP" + "\n")
-        self.out_file.write("A=M" + "\n")
-        self.out_file.write("M=D" + "\n")
-        self.out_file.write("@SP" + "\n")
-        self.out_file.write("M=M+1" + "\n")
+        cmd_block = []
+        cmd_block.extend([AT_SP, A_M, M_D, AT_SP, M_PLUS_1])
+        self.write_block(cmd_block)
+
 
     def _write_pop(self, segment, index):
         """
@@ -252,38 +382,40 @@ class CodeWriter:
         :param segment: The segment of the given pop command
         :param index: The index of the given pop command
         """
+        print(segment + index)
+        cmd_block = []
         if segment == TEMP or segment == POINTER:
             # calc dest of temp / pointer
             self._read_from_A(self.__ram_dict[segment] + int(index))
-            self.out_file.write("@" + CALC + "\n")
-            self.out_file.write("M=D" + "\n")
+            cmd_block.extend([AT+CALC, M_D])
+            self.write_block(cmd_block)
+
 
         elif segment == STATIC:
             # calc dest of static
             self._read_from_A(self.f_name + "." + str(index))
-            self.out_file.write("@" + CALC + "\n")
-            self.out_file.write("M=D" + "\n")
+            cmd_block.extend([AT+CALC, M_D])
+            self.write_block(cmd_block)
+
 
         else:
             # calc dest of segment
             self._read_from_address(str(self.__ram_dict[segment]))
-            self.out_file.write("@" + CALC + "\n")
-            self.out_file.write("M=D" + "\n")
-            self._read_from_A(index)
-            self.out_file.write("@" + CALC + "\n")
-            self.out_file.write("M=M+D" + "\n")
+            cmd_block.extend([AT+CALC, M_D])
+            self.write_block(cmd_block)
 
+            self._read_from_A(index)
+            cmd_block.clear()
+            cmd_block.extend([AT+CALC, M_PLUS_D])
+            self.write_block(cmd_block)
         self._pop_to_dest()
 
     def _pop_to_dest(self):
         # add commands for popping stack val to R13 address content
-        self.out_file.write("@SP" + "\n")
-        self.out_file.write("M=M-1" + "\n")
-        self.out_file.write("A=M" + "\n")
-        self.out_file.write("D=M" + "\n")
-        self.out_file.write("@" + CALC + "\n")
-        self.out_file.write("A=M" + "\n")
-        self.out_file.write("M=D" + "\n")
+        cmd_block = []
+        cmd_block.extend([AT_SP, M_MINUS_1, A_M, D_M, AT+CALC, A_M,M_D])
+        self.write_block(cmd_block)
+
 
     def _get_symbol(self, segment):
         """
@@ -299,13 +431,20 @@ class CodeWriter:
         base_address = CodeWriter.__ram_dict[segment]
         return str(int(base_address))
 
+    def write_block(self, cmd_block):
+        """
+        Writes a full block of assembly commands
+        :param cmd_block: A list containing all the block's assembly commands
+        """
+        for line in cmd_block:
+            self.out_file.write(line + "\n")
+#        self.out_file.write("\n")
+
     def close(self):
         """
         closes the CodeWriter's output file
         """
-        self.out_file.write("@END" + "\n")
-        self.out_file.write("0;JMP" + "\n\n")
-        self.out_file.write("(END)" + "\n")
-        self.out_file.write("@END" + "\n")
-        self.out_file.write("0;JMP" + "\n\n")
+        cmd_block = []
+        cmd_block.extend([AT_END, JMP, END, JMP])
+        self.write_block(cmd_block)
         self.out_file.close()
