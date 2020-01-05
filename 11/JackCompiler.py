9a908985b32fdd2916e0cc5d531335c32706b38d
Noa levitzky <45270253+noalevitzky@users.noreply.github.com>
2020-01-05 21:08:12 +0200

    Update JackCompiler.py

--- 11/JackCompiler.py
+++ 11/JackCompiler.py
@@ -3,6 +3,7 @@ import os
 import sys
 import CompilationEngine as Ce
 import JackTokenizer as Jt
+import VMWriter as Vw
 
 if __name__ == '__main__':
     source = sys.argv[1]
@@ -16,12 +17,18 @@ if __name__ == '__main__':
             if ext == '.jack':
                 # edit outfile name
                 outfile = source + '/' + name + '.vm'
+                # create VMWriter
+                vmWriter = Vw.Vm(outfile)
                 # create tokenizer
                 tokenizer = Jt.JackTokenizer(source + '/' + filename)
                 # compile
-                Ce.CompilationEngine(tokenizer, outfile)
+                
+                Ce.CompilationEngine(tokenizer, vmWriter)
     else:
         # source is a single file, compile it
         outfile = source.replace('.jack', '.vm')
+        # create VMWriter
+        vmWriter = Vw.Vm(outfile)
+        # create tokenizer
         tokenizer = Jt.JackTokenizer(source)
-        Ce.CompilationEngine(tokenizer, outfile)
+        Ce.CompilationEngine(tokenizer, vmWriter)
